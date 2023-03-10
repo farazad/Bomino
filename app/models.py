94af7bdb07ba761bac7ff79e8ea6dc6130140e65
farzad <farzaddarvish@yahoo.com>
2023-03-10 23:16:50 +0330

    define sumationRepo model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class SumationRepo(models.Model):
+    a = models.IntegerField(null=False)
+    b = models.IntegerField(null=False)
+    result = models.IntegerField()
+
+    @staticmethod
+    def get_total():
+        return SumationRepo.objects.aggregate(models.Sum('result'))['result__sum']

13f614c0fed9ef96b20a7addffb9d9010bb700a8
farzad <farzaddarvish@yahoo.com>
2023-03-10 23:17:42 +0330

    define app urls in Bomino_urls

--- Bomino/urls.py
+++ Bomino/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('app.urls')),
     path('admin/', admin.site.urls),
-    path('auth/', include('rest_framework.urls'))
+    path('auth/', include('rest_framework.urls')),
+
 ]

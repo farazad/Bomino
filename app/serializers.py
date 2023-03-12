2019d40dbabddec7b57273a858834bc933ae9123
Farzad <farzaddarvish@yahoo.com>
2023-03-12 12:44:08 +0330

    remove total serializer

--- app/serializers.py
+++ app/serializers.py
@@ -10,11 +10,3 @@ class HistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = SumationRepo
         fields = ['a', 'b']
-
-class TotalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SumationRepo
-        fields = ['result']
-
-    def to_representation(self, instance):
-        return super().to_representation(instance)
